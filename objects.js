2b11246386b16e169c2821eac7cd1e47106030d8
Diogo Costa <dsgmcosta@gmail.com>
2019-02-02 23:47:20 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = { dido: "thank you" }
 function updatePlaylist(playlist, artist, song) {
   playlist[artist] = song
 }
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+}
